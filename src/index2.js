133713fbc650905d48641e41d2914fa85ebb5a4a
isaias <isaarg2312@gmail.com>
2020-06-18 20:11:48 -0300

    isa changes

--- src/index2.js
+++ src/index2.js
@@ -1,22 +1,40 @@
-var contactos = JSON.parse(localStorage.getItem("contactosArray"));
-var name = document.getElementById("nombre");
-var tel = document.getElementById("telefono");
+const guardarContact = () => {
+  var contactos = JSON.parse(localStorage.getItem("contactosArray"));
+  var name = document.getElementById("nombre").value;
+  var tel = document.getElementById("telefono").value;
+  console.log(contactos);
+  let id = 0;
+  if (!contactos) {
+    contactos = [];
+    id = 1;
+  } else {         
+    id = contactos.length + 1;
+  }
+  var contacto = {
+    id: id,
+    nombre: name,
+    telefono: tel,
+  };
+  contactos.push(contacto);
+  localStorage.setItem("contactosArray", JSON.stringify(contactos));
+  
+  let newButton = document.createElement('button');
+  newButton.innerHTML = contacto.nombre;
+  newButton.setAttribute("onclick",`editContact(${id})`);
+  document.getElementById("name").appendChild(newButton);
+};
 
-guardarContact = () => {
-    if(contactos){
-        contactos = [];
-        var contacto = {
-            nombre: name,
-            telefono: tel,
-        };
-        contactos.push(contacto);
-        localStorage.setItem("contactosArray",JSON.stringify(contactos));
-        document.getElementById("name").innerHTML = nombre;
-    }
-}
+const verContact = () => {
+  document.getElementById("test").innerHTML = name;
+  document.getElementById("test2").innerHTML = tel;
+};
 
-verContact = () => {
-    document.getElementById("test").innerHTML = name;
-    document.getElementById("test2").innerHTML = tel;
-}
-
+const editContact = (id) => {
+    
+    let contactos = JSON.parse(localStorage.getItem("contactosArray"));
+    let contacto = contactos[id-1];
+    contacto.nombre = "isatest";
+    contacto.telefono = 1234;
+    contactos[id-1] = contacto; 
+    localStorage.setItem("contactosArray", JSON.stringify(contactos));
+};
