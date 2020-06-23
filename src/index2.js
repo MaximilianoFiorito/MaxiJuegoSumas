f7a4320f4ab4ccf9c587a79535a553f48dc3fd86
Your Name <you@example.com>
2020-06-23 18:06:07 -0300

    Agendav.2

--- src/index2.js
+++ src/index2.js
@@ -1,40 +1,62 @@
+const nombreh = "Nombre:"
+const telh = "Telefono:"
+const espacio = " "
 const guardarContact = () => {
-  var contactos = JSON.parse(localStorage.getItem("contactosArray"));
-  var name = document.getElementById("nombre").value;
-  var tel = document.getElementById("telefono").value;
+  let contactos = JSON.parse(localStorage.getItem("contactosArray"));
+  let name = document.getElementById("nombre").value;
+  let tel = document.getElementById("telefono").value;
   console.log(contactos);
-  let id = 0;
-  if (!contactos) {
-    contactos = [];
-    id = 1;
-  } else {         
-    id = contactos.length + 1;
-  }
-  var contacto = {
-    id: id,
-    nombre: name,
-    telefono: tel,
-  };
-  contactos.push(contacto);
-  localStorage.setItem("contactosArray", JSON.stringify(contactos));
-  
-  let newButton = document.createElement('button');
-  newButton.innerHTML = contacto.nombre;
-  newButton.setAttribute("onclick",`editContact(${id})`);
-  document.getElementById("name").appendChild(newButton);
-};
+  let id = document.getElementById("contacto_id").value;
+  /*Se crea un contacto*/
+  if (id === "") {
+    console.log(id)
+    if (!contactos) {
+      contactos = [];
+      id = 1;
+    } else {
+      id = contactos.length + 1;
+    }
+    let contacto = {
+      id: id,
+      nombre: name,
+      telefono: tel,
+    };
+    contactos.push(contacto);
+    localStorage.setItem("contactosArray", JSON.stringify(contactos));
 
-const verContact = () => {
-  document.getElementById("test").innerHTML = name;
-  document.getElementById("test2").innerHTML = tel;
+    let newButton = document.createElement('button');
+    newButton.innerHTML = nombreh + name + espacio + telh + tel;
+    newButton.setAttribute("class", "col-3");
+    newButton.setAttribute("onclick", `editContact(${id})`);
+    newButton.setAttribute("id", 'button_' + id);
+    document.getElementById("name").appendChild(newButton);
+  } else {
+    let contacto = {
+      id: id,
+      nombre: name,
+      telefono: tel,
+    };
+    contactos[id - 1] = contacto;
+    localStorage.setItem("contactosArray", JSON.stringify(contactos));
+    document.getElementById("button_" + id).innerHTML = nombreh + name + espacio + telh + tel;
+    document.getElementById("contacto_id").value = "";
+  }
 };
 
 const editContact = (id) => {
-    
-    let contactos = JSON.parse(localStorage.getItem("contactosArray"));
-    let contacto = contactos[id-1];
-    contacto.nombre = "isatest";
-    contacto.telefono = 1234;
-    contactos[id-1] = contacto; 
-    localStorage.setItem("contactosArray", JSON.stringify(contactos));
+  document.getElementById("contacto_id").value = id;
 };
+
+const mostrarContactos = () => {
+  let contactos = JSON.parse(localStorage.getItem("contactosArray"));
+  if (contactos) {
+    contactos.map((contacto) => {
+      let newButton = document.createElement('button');
+      newButton.innerHTML = nombreh + contacto.nombre + espacio + telh + contacto.telefono;
+      newButton.setAttribute("class", "col-3");
+      newButton.setAttribute("onclick", `editContact(${contacto.id})`);
+      document.getElementById("name").appendChild(newButton);
+      newButton.setAttribute("id", 'button_' + contacto.id);
+    })
+  }
+}
